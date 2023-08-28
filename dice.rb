6feaac2dfa7b95581650bcf7eaa6a64c5138b71b
Dmitrii Sumenko <64180264+flstudio4@users.noreply.github.com>
2023-08-28 01:00:19 +0000

    changes

--- dice.rb
+++ dice.rb
@@ -11,11 +11,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<h1>DICE ROLL</h1>
-  <a href='/dice/2/6'>Roll 2 6-sided dice</a><br>
-  <a href='/dice/2/10'>Roll 2 10-sided dice</a><br>
-  <a href='/dice/1/20'>Roll 1 20-sided dice</a><br>
-  <a href='/dice/5/4'>Roll 5 4-sided dice</a>"
+  erb(:elephant)
   end
 
 get("/zebra") do
@@ -30,11 +26,8 @@ get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
