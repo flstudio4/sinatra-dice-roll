7b8956dd1035b7b30b50464ceed20c7ff042010b
Dmitrii Sumenko <64180264+flstudio4@users.noreply.github.com>
2023-08-28 00:37:47 +0000

    changes

--- dice.rb
+++ dice.rb
@@ -5,6 +5,11 @@ require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
 
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 get("/") do
   "<h1>DICE ROLL</h1>
   <a href='/dice/2/6'>Roll 2 6-sided dice</a><br>
